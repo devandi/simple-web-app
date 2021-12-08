586647ebf463fd6da8dc4f2c74c231de51b01abe
Andreas Wagner <mailbox.wagner@gmail.com>
2021-12-08 10:31:49 +0100

    Init commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8080", nil)
+}
